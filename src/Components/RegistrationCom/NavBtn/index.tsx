da397080c6d3f1ea79179e960dfc52969e42a71f
AleksiDu <aleksi.duluzauri@gmail.com>
2023-03-26 12:50:37 +0400

    add responsive tabs for register

--- src/Components/RegistrationCom/NavBtn/index.tsx
+++ src/Components/RegistrationCom/NavBtn/index.tsx
@@ -1,16 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
 
-const NavBtn = (props: { name: string }) => {
+interface NavBtnProps {
+  name: string;
+  activeBtn: string;
+  onClick: (btnName: string) => void;
+}
+
+const NavBtn = (props: NavBtnProps) => {
   const REGISTER_URL = `/registration`;
   const navigate = useNavigate();
+
   const handleClick = () => {
-    navigate(REGISTER_URL + `/${props.name}`);
+    const newActiveBtn = props.activeBtn === props.name ? "" : props.name;
+    props.onClick(newActiveBtn);
+    navigate(newActiveBtn ? REGISTER_URL + `/${newActiveBtn}` : REGISTER_URL);
   };
+
+  const buttonClassName = `${styles.btn} ${styles[props.name]} ${
+    props.activeBtn === props.name ? styles.active : ""
+  }`;
 
   return (
     <div className={styles.div}>
-      <button className={styles.btn} onClick={handleClick}>
+      <button className={buttonClassName} onClick={handleClick}>
         {props.name}
       </button>
     </div>
--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBtn from "../../Components/RegistrationCom/NavBtn";
 import RegisterForm from "../../Components/RegistrationCom/RegisterForm";
 
 const Registration = () => {
+  const [activeBtn, setActiveBtn] = useState("");
+
+  const handleBtnClick = (btnName: string) => {
+    const newActiveBtn = activeBtn === btnName ? "" : btnName;
+    setActiveBtn(newActiveBtn);
+  };
+
   return (
     <div>
-      <NavBtn name="Tutor" />
-      <NavBtn name="Student" />
-      <NavBtn name="Parent" />
+      <NavBtn name="Tutor" activeBtn={activeBtn} onClick={handleBtnClick} />
+      <NavBtn name="Student" activeBtn={activeBtn} onClick={handleBtnClick} />
+      <NavBtn name="Parent" activeBtn={activeBtn} onClick={handleBtnClick} />
 
       <Routes>
         <Route path="/Tutor" element={<RegisterForm name="Tutor" />} />
