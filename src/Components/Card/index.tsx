b81a3e072fcc07fd0dedd3f9b118cd8efbb1be81
AleksiDu <aleksi.duluzauri@gmail.com>
2023-05-19 21:24:53 +0400

    add foter handlers for social and donate

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -3,22 +3,41 @@ interface CardProps {
   header?: string;
   paragraph?: string;
   author?: string;
+  cardClass?: string;
   items?: { id: string; text: string }[];
+  onClick?: (id: string) => void;
 }
 const Card: React.FC<CardProps> = ({
   header,
   paragraph,
   author,
   items,
+  cardClass,
   className,
+  onClick,
 }) => {
+  const handleItemClick = (id: string) => {
+    if (onClick) {
+      onClick(id); // Pass the id to the onClick prop
+    }
+  };
+
   return (
     <div className={className || "1"}>
       <h2>{header}</h2>
       <p>{paragraph}</p>
       <span>{author}</span>
       <ul>
-        {items && items.map((item) => <li key={item.id}>{item.text}</li>)}
+        {items &&
+          items.map((item) => (
+            <li
+              key={item.id}
+              className={cardClass}
+              onClick={() => handleItemClick(item.id)}
+            >
+              {item.text}
+            </li>
+          ))}
       </ul>
     </div>
   );
--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -9,6 +9,28 @@ const Footer: React.FC = () => {
     { id: "3", text: "Email" },
   ];
 
+  const donate = [
+    { id: "1", text: "BOG" },
+    { id: "2", text: "TBC" },
+    { id: "3", text: "PayPal" },
+  ];
+
+  const socialHandler = (id: string) => {
+    const clickedItem = contact.find((item) => item.id === id);
+    if (clickedItem) {
+      // TODO add links for social
+      console.log(clickedItem.text);
+    }
+  };
+
+  const donateHandler = (id: string) => {
+    const clickedItem = donate.find((item) => item.id === id);
+    if (clickedItem) {
+      // TODO add links for donate
+      console.log(clickedItem.text);
+    }
+  };
+
   return (
     <footer className={styles.footer}>
       <section className={styles.container}>
@@ -18,8 +40,20 @@ const Footer: React.FC = () => {
           paragraph="Pedagogue is a nonprofit organization that consists of an
             interactive learning platform."
         />
-        <Card className={styles.column} header="Contact" items={contact} />
-        <Card className={styles.column} header="Support" paragraph="Help us." />
+        <Card
+          className={styles.column}
+          header="Contact"
+          items={contact}
+          cardClass={styles.social}
+          onClick={socialHandler}
+        />
+        <Card
+          className={styles.column}
+          header="Support"
+          items={donate}
+          cardClass={styles.social}
+          onClick={donateHandler}
+        />
       </section>
       <div className={styles.bottom}>
         <p>&copy; {new Date().getFullYear()} Pedagogue. All rights reserved.</p>
