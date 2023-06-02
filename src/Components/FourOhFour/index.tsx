05fb46b007a65742fd464305cabe5c5763a0e317
AleksiDu <aleksi.duluzauri@gmail.com>
2023-06-02 18:03:51 +0400

    add add animation for 404

--- src/Components/FourOhFour/index.tsx
+++ src/Components/FourOhFour/index.tsx
@@ -1,10 +1,45 @@
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import notFoundLogo from "../../assets/404.svg";
 
-const FourOhFour = () => {
+const FourOhFour: React.FC = () => {
+  const [displayText, setDisplayText] = useState("");
+  const text = "404 Page Not Found";
+
+  useEffect(() => {
+    let currentIndex = 0;
+
+    const typingInterval = setInterval(() => {
+      if (currentIndex === text.length) {
+        clearInterval(typingInterval);
+        if (text.length > 17) {
+          setTimeout(() => {
+            setDisplayText("");
+          }, 1000);
+        }
+        return;
+      }
+
+      setDisplayText((prevText) => text.substring(0, currentIndex + 1));
+
+      currentIndex++;
+    }, 150);
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, []);
+
+  const handleReset = () => {
+    setDisplayText("");
+  };
+
   return (
-    <div className="four-oh-four">
-      <img alt={"not-found"} src={notFoundLogo} />
+    <div className="four-oh-four-container">
+      <img alt="not found logo" src={notFoundLogo} />
+      <h1 className="typing-text">{displayText}</h1>
+      <p>Oops! The page you are looking for does not exist.</p>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 };
