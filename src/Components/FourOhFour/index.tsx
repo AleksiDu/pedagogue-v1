398dacb678450d9d6b75fe86623af0da271ddb53
AleksiDu <aleksi.duluzauri@gmail.com>
2023-06-03 14:37:06 +0400

    fix clear interval function

--- src/Components/FourOhFour/index.tsx
+++ src/Components/FourOhFour/index.tsx
@@ -12,7 +12,7 @@ const FourOhFour: React.FC = () => {
     const typingInterval = setInterval(() => {
       if (currentIndex === text.length) {
         clearInterval(typingInterval);
-        if (text.length > 17) {
+        if (text.length > 18) {
           setTimeout(() => {
             setDisplayText("");
           }, 1000);
@@ -20,7 +20,7 @@ const FourOhFour: React.FC = () => {
         return;
       }
 
-      setDisplayText((prevText) => text.substring(0, currentIndex + 1));
+      setDisplayText(() => text.substring(0, currentIndex + 1));
 
       currentIndex++;
     }, 150);
@@ -30,16 +30,11 @@ const FourOhFour: React.FC = () => {
     };
   }, []);
 
-  const handleReset = () => {
-    setDisplayText("");
-  };
-
   return (
     <div className="four-oh-four-container">
       <img alt="not found logo" src={notFoundLogo} />
       <h1 className="typing-text">{displayText}</h1>
       <p>Oops! The page you are looking for does not exist.</p>
-      <button onClick={handleReset}>Reset</button>
     </div>
   );
 };
